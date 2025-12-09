7fc09f65223df3f06238f53d259560a7d7d5616a
930606miketang <tangxinquan66@gmail.com>
2025-12-08 21:51:57 -0500

    drumset

---
+++ example.js
@@ -0,0 +1,16 @@
+let x = 3;
+
+
+function myexample() {
+    console.log("hi");
+}
+
+let x = "im a string";
+
+let y =5 // number
+let z =3.8 //number
+
+let j = true; // boolean
+
+let a = null; // null
+let b = undefined // undefined - dont do !
---
+++ example.py
@@ -0,0 +1,14 @@
+x = 3 
+
+
+def myexample():
+    print ("hi")
+
+
+    x = "im a string"
+
+    y =5 # int
+    z = 3.8# float
+j = True # bool
+
+a = None #Nonetype
